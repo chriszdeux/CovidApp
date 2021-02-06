ceeb9a3a055a945df4ef9de1dd538f2a665c8d1b
chriszdeux <chriszdeux@gmail.com>
2021-02-06 15:41:29 -0600

    All set

---
+++ src/CovidApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CovidApp = () => {
+  return (
+    <>
+      <h1>Covid App 2021</h1>
+    </>
+  )
+}
---
+++ src/components/header/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <h2>CovidApp</h2>
+        
+      </nav>
+    </header>
+  )
+}
